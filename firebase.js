b0bc2e008e499013ab71ccbb2a732ed49f18875e
aqmalLauzit <maqmalh2001@gmail.com>
2023-03-01 19:17:35 +0700

    First commit sampe login google non front

---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY ,
+  authDomain: "twitter-clonev2.firebaseapp.com",
+  projectId: "twitter-clonev2",
+  storageBucket: "twitter-clonev2.appspot.com",
+  messagingSenderId: "348067378008",
+  appId: "1:348067378008:web:e1db4a104e61c7e6eeab2d"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) :getApp();
+const db = getFirestore();
+const storage = getStorage();
+export {
+    app, db, storage
+};
