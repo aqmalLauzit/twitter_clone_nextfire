ce13d9d79425fd2a702a334832eae5d75b3f35ff
aqmalLauzit <maqmalh2001@gmail.com>
2023-03-04 19:47:30 +0700

    tambah fungsi like

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,3 +1,4 @@
+
 import {
   ChartBarIcon,
   ChatIcon,
@@ -6,9 +7,48 @@ import {
   ShareIcon,
   TrashIcon,
 } from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid"; 
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Moment from "react-moment";
+import { db } from "../firebase";
 
 export default function Post({ post }) {
+
+  const {data : session} = useSession();
+  const router = useRouter();
+  const [likes,setLikes] = useState([]);
+  const [hasLiked,setHasLiked] = useState(false);
+
+  useEffect(() => {
+    const unsubcribe = onSnapshot(
+      collection(db,"posts", post.id, "likes"),(snapshot) => setLikes(snapshot.docs)
+    )
+  },[db])
+
+  useEffect(() => {
+    setHasLiked(likes.findIndex((like) => like.id === session?.user?.uid) !== -1)
+  }, [likes])
+
+  const likePost = async() => {
+    if(session) {
+      if(hasLiked){
+        await deleteDoc(doc(db,"posts",post.id,"likes",session?.user?.uid))
+      } else {
+  
+        //menambahkan relasi satu postingan di likes oleh multiple user
+        await setDoc(doc(db,"posts",post.id,"likes",session?.user?.uid ),{
+          username: session?.user?.username
+        })
+      }
+    } else {
+      router.push("/auth/signin");
+    }
+
+    
+  }
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
       {/* user image */}
@@ -31,7 +71,7 @@ export default function Post({ post }) {
             <span className="text-sm sm:text-[15px] hover:underline">
               <Moment fromNow>
 
-              {post?.timestamp?.toDate()}
+              {post?.data().timestamp?.toDate()}
               </Moment>
             </span>
             <span className="text-sm sm:text-[15px] col">@{post.data().username} - </span>
@@ -56,7 +96,22 @@ export default function Post({ post }) {
         <div className="flex justify-between text-gray-500 p-2">
           <ChatIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <TrashIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
-          <HeartIcon className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          <div className="flex items-center">
+
+          
+          {hasLiked ? (
+
+          <HeartIconFilled onClick={likePost} className=" text-red-600 bg-red-100 h-9 w-9 hoverEffect p-2 hover:text-red-100 hover:bg-black" />
+          ) : (
+
+          <HeartIcon onClick={likePost} className="h-9 w-9 hoverEffect p-2 hover:text-red-600 hover:bg-red-100" />
+          )}
+          {
+            likes.length > 0 && (
+              <span className={`${hasLiked && 'text-red-600' }`}>{likes.length}</span>
+            )
+          }
+</div>
           <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
           <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
